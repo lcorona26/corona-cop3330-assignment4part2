9f5c889cf101f672c743e99b06b9dd95d42627be
lcorona26 <84474226+lcorona26@users.noreply.github.com>
2021-07-11 21:56:34 -0400

    Update LocalItem.java

--- src/main/java/ucf/assignments/LocalItem.java
+++ src/main/java/ucf/assignments/LocalItem.java
@@ -1,9 +1,10 @@
-package ucf.assignments;
-
 /*
  *  UCF COP3330 Summer 2021 Assignment 4 Solution
  *  Copyright 2021 Luis Corona
  */
+
+package ucf.assignments;
+
 
 import java.time.LocalDate;
 
